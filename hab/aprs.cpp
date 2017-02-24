13a715aee9e02f53e07eeb67c3319378dfd9db30
AG6GR <slhe@princeton.edu>
2017-02-24 17:41:45 -0500

    Update GPS code

--- hab/aprs.cpp
+++ hab/aprs.cpp
@@ -1,150 +1,137 @@
 /*
- * Copyright (C) 2013 by KC3ARY Rich Nash
- * 
- * Module modified after being inheritted from EA5HAV. The same license described below
- * is in effect.
- *
- * The below copyright is kept intact.
- *
- */
+* Copyright (C) 2017 by AG6GR
+* 
+* Module modified after being inheritted from KC3ARY Rich Nash and EA5HAV. The same license described below
+* is in effect.
+*
+* The below copyright is kept intact.
+*
+*/
 
 /* Credit to the trackuino project written by EA5HAV Javi
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- */
+*
+* This program is free software; you can redistribute it and/or
+* modify it under the terms of the GNU General Public License
+* as published by the Free Software Foundation; either version 2
+* of the License, or (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include <WProgram.h>
 #include "afsk.h"
 #include "ax25.h"
 #include "aprs.h"
+#include "TinyGPS++.h"
 
 #define MAXSENDBUFFER 500 // Used to allocate a static buffer on the stack to build the AX25 buffer
 
 uint16_t preambleFlags;
 
-// Convert latitude from a float to a string
-void latToStr(char * const s, const int size, float lat)
+// Convert latitude from a float to a string DDMM.MM[N/S]
+void latToStr(char * const s, const int size, RawDegrees latitude)
 {
-  char hemisphere = 'N';
-  if (lat < 0) {
-    lat = -lat;
-    hemisphere = 'S';
-  }
-  const int deg = (int) lat;
-  lat = (lat - (float) deg) * 60.0f;
-  const int min = (int) lat;
-  lat = (lat - (float) min) * 100.0f;
-  const int minTenths = (int) (lat + 0.5); // Round the tenths
-  snprintf(s, size, "%02d%02d.%02d%c", deg, min, minTenths, hemisphere);
+	char hemisphere = latitude.negative ? "S" : "N";
+	// TODO verify if minutes includes whole degrees
+	uint32_t milli_minutes = 60 * latitude.billionths / 10000000;
+	snprintf(s, size, "%02d%02d.%02d%c", latitude.deg, milli_minutes / 100, milli_minutes % 100, hemisphere);
 }
 
-// Convert latitude from a float to a string
-void lonToStr(char * const s, const int size, float lon)
+// Convert latitude from a float to a string DDDMM.MM[W/E]
+void lonToStr(char * const s, const int size, RawDegrees longitude)
 {
-  char hemisphere = 'E';
-  if (lon < 0) {
-    lon = -lon;
-    hemisphere = 'W';
-  }
-  const int deg = (int) lon;
-  lon = (lon - (float) deg) * 60.0f;
-  const int min = (int) lon;
-  lon = (lon - (float) min) * 100.0f;
-  const int minTenths = (int) (lon + 0.5); // Round the tenths
-  snprintf(s, size, "%03d%02d.%02d%c", deg, min, minTenths, hemisphere);
+	char hemisphere = longitude.negative ? "W" : "E";
+	
+	// TODO verify if minutes includes whole degrees
+	uint32_t milli_minutes = 60 * longitude.billionths / 10000000;
+	snprintf(s, size, "%03d%02d.%02d%c", longitude.deg, milli_minutes / 100, milli_minutes % 100, hemisphere);
 }
 
 // Dump out the AX25 packet in a semi-readable way
 // Note, the end of header and CRC are sent as ASCII characters, which they aren't
-void logBuffer(const uint8_t * const buf, const int bitsSent,
-    const uint8_t dayOfMonth, const uint8_t hour, const uint8_t min)
+void logBuffer(const uint8_t * const buf, const int bitsSent)
 {
-  Serial.printf("Bits in packet %d: ", bitsSent);
-  Serial.print(dayOfMonth);
-  Serial.print(',');
-  Serial.print(hour);
-  Serial.print(',');
-  Serial.print(min);
-  Serial.print(',');
-
-  uint8_t frameState = 0; // 0-No start, 1-start, 2-in header, 3-In info
-  uint8_t bSoFar = 0x00;
-  uint8_t gotBit = 0;
-  int numOnes = 0;
-  for (int onBit = 0; onBit < bitsSent; onBit++) {
-    uint8_t bit = buf[onBit >> 3] & (1 << (onBit & 7));
-    if (numOnes == 5) {
-      // This may be a 0 due to bit stuffing
-      if (bit) { // Maybe it's a 0x7e
-        onBit++;
-        bit = buf[onBit >> 3] & (1 << (onBit & 7));
-        if (gotBit == 6 && bSoFar == 0x3e && !bit) {
-          // Got 0x7e frame start/end
-          if (frameState == 0) {
-            frameState = 1;
-            Serial.print('[');
-          } else
-            if (frameState == 3 || frameState == 2) {
-              frameState = 0;
-              Serial.print(']');
-            }
-          bSoFar = 0x00;
-          gotBit = 0;
-        } else {
-          Serial.print('X'); // Error
-        }
-      }
-      numOnes = 0;
-      continue;
-    }
-    if (bit) {
-      // Set the one bit
-      bSoFar |= (1 << gotBit);
-      numOnes++;
-    } else {
-      numOnes = 0;
-    }
-    gotBit++;
-    if (gotBit == 8) {
-      // Got a byte;
-      if (frameState == 1) {
-        frameState = 2;
-      } else
-        if (frameState == 2 && bSoFar == 0xf0) { // 0xf0 is the last byte of the header
-          frameState = 3;
-        }
-      if (frameState == 2) {
-        // In header
-        Serial.print((char) (bSoFar >> 1));
-      } else {
-        // In info
-        Serial.print((char) bSoFar);
-      }
-      bSoFar = 0x00;
-      gotBit = 0;
-    }
-  }
-  Serial.println();
+	Serial.printf("Bits in packet %d: ", bitsSent);
+	//Serial.print(dayOfMonth);
+	//Serial.print(',');
+	//Serial.print(hour);
+	//Serial.print(',');
+	//Serial.print(min);
+	//Serial.print(',');
+	
+	uint8_t frameState = 0; // 0-No start, 1-start, 2-in header, 3-In info
+	uint8_t bSoFar = 0x00;
+	uint8_t gotBit = 0;
+	int numOnes = 0;
+	for (int onBit = 0; onBit < bitsSent; onBit++) {
+		uint8_t bit = buf[onBit >> 3] & (1 << (onBit & 7));
+		if (numOnes == 5) {
+			// This may be a 0 due to bit stuffing
+			if (bit) { // Maybe it's a 0x7e
+				onBit++;
+				bit = buf[onBit >> 3] & (1 << (onBit & 7));
+				if (gotBit == 6 && bSoFar == 0x3e && !bit) {
+					// Got 0x7e frame start/end
+					if (frameState == 0) {
+						frameState = 1;
+						Serial.print('[');
+					} else
+					if (frameState == 3 || frameState == 2) {
+						frameState = 0;
+						Serial.print(']');
+					}
+					bSoFar = 0x00;
+					gotBit = 0;
+				} else {
+					Serial.print('X'); // Error
+				}
+			}
+			numOnes = 0;
+			continue;
+		}
+		if (bit) {
+			// Set the one bit
+			bSoFar |= (1 << gotBit);
+			numOnes++;
+		} else {
+			numOnes = 0;
+		}
+		gotBit++;
+		if (gotBit == 8) {
+			// Got a byte;
+			if (frameState == 1) {
+				frameState = 2;
+			} else
+			if (frameState == 2 && bSoFar == 0xf0) { // 0xf0 is the last byte of the header
+				frameState = 3;
+			}
+			if (frameState == 2) {
+				// In header
+				Serial.print((char) (bSoFar >> 1));
+			} else {
+				// In info
+				Serial.print((char) bSoFar);
+			}
+			bSoFar = 0x00;
+			gotBit = 0;
+		}
+	}
+	Serial.println();
 }
 
 void aprs_setup(const uint16_t p_preambleFlags, const uint8_t pttPin,
     const uint16_t pttDelay, const uint32_t toneLength,
     const uint32_t silenceLength)
 {
-  preambleFlags = p_preambleFlags;
-  afsk_setup(pttPin, pttDelay, toneLength, silenceLength);
+	preambleFlags = p_preambleFlags;
+	afsk_setup(pttPin, pttDelay, toneLength, silenceLength);
 }
 
 // This can operate in two modes, PTT or VOX
@@ -154,64 +141,88 @@ void aprs_setup(const uint16_t p_preambleFlags, const uint8_t pttPin,
 // In PTT mode the pin given will be raised high, and then PTT_DELAY ms later, the packet will
 // begin
 void aprs_send(const PathAddress * const paths, const int nPaths,
-    const uint8_t dayOfMonth, const uint8_t hour, const uint8_t min,
-    const float lat,
-    const float lon, // degrees
-    const float altitude, // meters
-    const uint16_t heading, // degrees
-    const float speed, const char symbolTableIndicator, const char symbol,
+	TinyGPSPlus &gps,
+	const char symbolTableIndicator, const char symbol,
     const char * const comment)
 {
-  uint8_t buf[MAXSENDBUFFER];
-  char temp[12];
-
-  ax25_initBuffer(buf, sizeof(buf));
-
-  ax25_send_header(paths, nPaths, preambleFlags);
-
-  ax25_send_byte('/'); // Report w/ timestamp, no APRS messaging. $ = NMEA raw data
-  snprintf(temp, sizeof(temp), "%02u%02u%02uz", (unsigned int) dayOfMonth,
-      (unsigned int) hour, (unsigned int) min);
-  ax25_send_string(temp);
-
-  latToStr(temp, sizeof(temp), lat);
-  ax25_send_string(temp);             // Lat:
-
-  ax25_send_byte(symbolTableIndicator);           // Which Symbol table to use
-
-  lonToStr(temp, sizeof(temp), lon);
-  ax25_send_string(temp);     // Lon: 000deg and 25.80 min
-
-  ax25_send_byte(symbol);  // The symbol
-
-  snprintf(temp, sizeof(temp), "%03u", heading);
-  ax25_send_string(temp);             // Heading (degrees)
-
-  ax25_send_byte('/');                // and
-
-  snprintf(temp, sizeof(temp), "%03d", (unsigned int) (speed + 0.5));
-  ax25_send_string(temp);             // speed (knots)
-
-  ax25_send_string("/A="); // Altitude (meters). Goes anywhere in the comment area
-
-  snprintf(temp, sizeof(temp), "%06ld", (long) altitude);
-  ax25_send_string(temp);
-
-  ax25_send_string(comment);     // Comment
-
-  ax25_send_footer();
-
-  Serial.flush(); // Make sure all Serial data (which is based on interrupts) is done before you start sending.
-
-  // Set the buffer of bits we are going to send
-  afsk_set_buffer(buf, ax25_getPacketSize());
-
-  Serial.flush(); // Wait until all characters are sent
-
-  // OK, no more operations until this is done.
-  afsk_start();
-  while (afsk_busy())
-    ;
-
-  logBuffer(buf, ax25_getPacketSize(), dayOfMonth, hour, min);
+	uint8_t buf[MAXSENDBUFFER];
+	char temp[12];
+	
+	ax25_initBuffer(buf, sizeof(buf));
+	
+	ax25_send_header(paths, nPaths, preambleFlags);
+	
+	// ----- Begin Comment/Data section ----- //
+	
+	// Data type flag byte
+	// '/': Report w/ timestamp, no APRS messaging. 
+	// '$': NMEA raw data
+	ax25_send_byte('/');
+	
+	// Timestamp HHMMSS format, append h to indicate HMS format
+	snprintf(temp, sizeof(temp), "%02u%02u%02uh", (unsigned int) gps.time.hour(),
+		(unsigned int) gps.time.minute(), (unsigned int) gps.time.second());
+	ax25_send_string(temp);
+	
+	// ----- Position Data ----- //
+	
+	// Example location block: "4903.50N/07201.75W-", using Symbol Table ID '/' and Code '-'
+	
+	// Latitude DDMM.MM
+	latToStr(temp, sizeof(temp), gps.location.rawLat());
+	ax25_send_string(temp);
+	
+	// Display Symbol Table ID
+	ax25_send_byte(symbolTableIndicator);
+	
+	// Longitude DDDMM.MM
+	lonToStr(temp, sizeof(temp), gps.location.rawLng());
+	ax25_send_string(temp);     // Lon: 000deg and 25.80 min
+	
+	// Display Symbol Code
+	ax25_send_byte(symbol);
+	
+	// ----- APRS Data Extension CSE + '/' + SPD (7b) ----- //
+	// Heading (degrees, 3b)
+	snprintf(temp, sizeof(temp), "%03u", (unsigned int)gps.course.deg());
+	ax25_send_string(temp);
+	
+	// Separator
+	ax25_send_byte('/');
+	
+	// Speed (knots)
+	snprintf(temp, sizeof(temp), "%03d", (unsigned int) gps.speed.knots());
+	ax25_send_string(temp);
+	
+	// Altitude /A=aaaaaa, must be in feet. 
+	ax25_send_string("/A="); 
+	snprintf(temp, sizeof(temp), "%06d", (unsigned int)gps.altitude.feet());
+	ax25_send_string(temp);
+	
+	// ----- Additional Comments ----- //
+	
+	
+	// Extra comments
+	ax25_send_string(comment);     
+	
+	// Footer
+	ax25_send_footer();
+	
+	// ----- Begin transmission code ----- //
+	// Make sure all Serial data (which is based on interrupts) is done before you start sending.
+	Serial.flush(); 
+	
+	// Set the buffer of bits we are going to send
+	afsk_set_buffer(buf, ax25_getPacketSize());
+	
+	// Double check all characters are sent
+	Serial.flush(); 
+	
+	// Critical section, no more operations until this is done.
+	afsk_start();
+	while (afsk_busy())
+		;
+	
+	// Debug logging
+	logBuffer(buf, ax25_getPacketSize());
 }
