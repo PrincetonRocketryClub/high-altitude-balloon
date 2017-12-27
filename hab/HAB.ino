4da79653e280276e26688deb98a90011b55e4f48
AG6GR <slhe@princeton.edu>
2017-12-27 11:49:44 -0800

    Fix copyrights

--- hab/HAB.ino
+++ hab/HAB.ino
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2014 by Richard Nash (KC3ARY)
+* Copyright (C) 2017 by Sunny He (AG6GR)
 * 
 * This program is free software; you can redistribute it and/or
 * modify it under the terms of the GNU General Public License
